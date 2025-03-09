8490f8437c429e6bd412f93a85ea4f045efde897
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-03-09 23:33:00 +0500

    each:loop

---
+++ src/Conditionals/Each_loop.java
@@ -0,0 +1,11 @@
+package Conditionals;
+
+public class Each_loop {
+    public static void main(String[] args)
+    {
+        int[] numbers = {3, 7, 5, -5};
+        for (int number : numbers){
+            System.out.println(number);
+        }
+    }
+}
