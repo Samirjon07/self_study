252ee0b46f14fc8720df4b3d6bbbe1127c3703fe
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-03-11 23:16:39 +0500

    letter pyramid

--- src/Conditionals/One_side_letter.java
+++ src/Conditionals/One_side_letter.java
@@ -2,6 +2,15 @@ package Conditionals;
 
 public class One_side_letter {
     public static void main(String[] args) {
+        char last = 'E', alphabet = 'A';
 
+        for (int i = 1; i <= (last - 'A' + 1); ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print(alphabet + " ");
+            }
+            ++alphabet;
+
+            System.out.println();
+        }
     }
 }
