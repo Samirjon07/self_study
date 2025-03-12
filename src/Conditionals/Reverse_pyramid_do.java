854cf36c2cf6c638af40c1b708933a6789c5216f
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-03-12 19:51:15 +0500

    Pyramid

---
+++ src/Conditionals/Reverse_pyramid_do.java
@@ -0,0 +1,25 @@
+package Conditionals;
+
+public class Reverse_pyramid_do {
+    public static void main(String[] args) {
+
+        int j = 7;
+        do {
+            int i = 1;
+            do {
+                System.out.print("*");
+                i++;
+            } while (i <= j);
+            System.out.print("\n");
+            int k = 0;
+            do {
+                System.out.print(" ");
+                k += 2;
+            } while (k <= (7 - j));
+            if (j <= 1) {
+                break;
+            }
+            j -= 2;
+        } while (j <= 7);
+    }
+}
--- src/Conditionals/Reverse_pyramid_while.java
+++ src/Conditionals/Reverse_pyramid_while.java
@@ -1,4 +1,27 @@
 package Conditionals;
 
 public class Reverse_pyramid_while {
+    public static void main(String[] args)
+    {
+        int j=7;
+        while(j<=7){
+            int i=1;
+            while(i<=j){
+                System.out.print("*");
+                i++;
+            }
+            System.out.print("\n");
+            int k=0;
+            while(k<=(7-j)){
+                System.out.print(" ");
+                k+=2;
+            }
+            j-=2;
+            if (j<1){
+                break;
+            }
+
+        }
+    }
 }
+
