3b60c4d7b899febfe07a79291bc4b910fcdaed79
Tursunov Samirjon <124480604+Samirjon07@users.noreply.github.com>
2025-03-13 20:40:55 +0500

    Divisible

--- src/Conditionals/Midterm/From_1_30_divisible_to_2_5.java
+++ src/Conditionals/Midterm/From_1_30_divisible_to_2_5.java
@@ -1,4 +1,14 @@
 package Conditionals.Midterm;
 
 public class From_1_30_divisible_to_2_5 {
+    public static void main(String[] args) {
+
+        for (int i = 1;i<30;i++){
+            if (i<=25) {
+                if (i % 2 == 0 || i % 5 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
 }
